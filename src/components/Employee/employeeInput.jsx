c275b18a7ea5f110586b5318e359cd893d8f2144
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2023-08-11 14:01:35 +0630

    hrm

--- src/components/Employee/employeeInput.jsx
+++ src/components/Employee/employeeInput.jsx
@@ -1,6 +1,6 @@
 import { Input } from "@nextui-org/react";
 import {RadioGroup, Radio} from "@nextui-org/react";
-
+import {Textarea} from "@nextui-org/react";
 export default function employeeInput() {
   return (
     <div className="w-full flex flex-col gap-4">
@@ -80,6 +80,38 @@ export default function employeeInput() {
           placeholder=" "
           labelPlacement="outside"
         />
+      </div>
+      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+<Textarea
+      isReadOnly
+      label="Address"
+      variant="bordered"
+      labelPlacement="outside"
+      placeholder="Enter your address"
+     
+      className="max-w-xs"
+    />
+      </div>
+       
+        <Input
+          type="text"
+          label="Interview Result"
+          placeholder=" "
+          labelPlacement="outside"
+        />
+      </div>
+      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+        <Input
+          type="number"
+          label="Phone No"
+          placeholder=" "
+          labelPlacement="outside"
+        />
+        <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+            
+        </div>
+        
       </div>
       
     </div>
--- src/pages/Employee/employeeList.jsx
+++ src/pages/Employee/employeeList.jsx
@@ -11,11 +11,11 @@ export default function Employee() {
         <div className='flex'>
             <div className="sidebar"><Sidebar /></div>
             <div className="py-4">
-                <div className="head bg-white rounded-lg shadow-md min-w-[1240px]">  <Nav /></div>
+                <div className="head bg-white rounded-lg shadow-md min-w-[1080px]">  <Nav /></div>
                 <div className="body  py-1">
                     <Card className="rounded-md shadow-md py-3">
                         <CardHeader className="justify-between">
-                        <div>Back</div>
+                        <div></div>
                             <div className='font'>Employee List</div>
                             <Link to='/emp-add'> <Button size='sm' radius="sm" className="bg-gradient-to-tr from-pink-500 to-green-500 text-white shadow-sm ">
       Add
