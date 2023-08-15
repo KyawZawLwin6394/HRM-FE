a0746130bc4eb0b2b640a9d079cf88e760066a74
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2023-08-15 12:10:51 +0630

    hrm

--- src/components/Employee/employeeInput.jsx
+++ src/components/Employee/employeeInput.jsx
@@ -2,57 +2,111 @@ import { Input } from "@nextui-org/react";
 import { RadioGroup, Radio } from "@nextui-org/react";
 import {Modal, Button, useDisclosure} from "@nextui-org/react"
 import OtherDoc from './otherDocInput'
-import {useRef} from 'react'
-import {useNavigate} from 'react-router-dom'
+import {useRef,useState} from 'react'
+
 import apiInstance from '../../util/api.js'
+import  Swal  from 'sweetalert2';
+
 export default function EmployeeInput() {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
   const variant = ["faded"];
-    const navigate = useNavigate()
+
   const emailRef = useRef()
   const passRef = useRef()
   const nrcRef=useRef()
-  
+  const nameRef=useRef()
+  const addressRef=useRef()
+  // const ageRef=useRef()
+  const DOBRef=useRef()
+  const ECRef=useRef()
+  const phoneRef=useRef()
+  const passportRef=useRef()
+  const EuBackRef=useRef()
+  const EuCerRef=useRef()
+  const workExpRef=useRef()
+  const [cv,setCV]=useState(null)
+  // const otheRef=useRef()
+  const recLetterRef=useRef()
+  const firstInRef=useRef()
+  const firstResRef=useRef()
+  const secInRef=useRef()
+  const secResRef=useRef()
+  const fatherRef=useRef()
+  const empDateRef=useRef()
+  const genderRef=useRef()
 
+const handlefile=(e)=>{
+  if(e.target.files){
+    setCV(e.target.files[0])
+    console.log(e.target.files,'file')
+  }
+}
   //array list
   // const [userList,setUserList]=useState([])
   const create = () => {
-
+console.log(cv,'cv')
     const data = {
+      givenName:nameRef.current.value,
       email: emailRef.current.value,
       password: passRef.current.value,
-      NRC:nrcRef.current.value
+      NRC:nrcRef.current.value,
+      address:addressRef.current.value,
+      DOB:DOBRef.current.value,
+      emergencyContact:ECRef.current.value,
+      phone:phoneRef.current.value,
+      passportNo:passportRef.current.value,
+      educationBackground:EuBackRef.current.value,
+      educationCertificate:EuCerRef.current.value,
+      workExperience:workExpRef.current.value,
+      cv:cv,
+      recommendationLetter:recLetterRef.current.value,
+      firstInterviewDate:firstInRef.current.value,
+      firstInterviewResult:firstResRef.current.value,
+      secondInterviewDate:secInRef.current.value,
+      secondInterviewResult:secResRef.current.value,
+      fatherName:fatherRef.current.value,
+      gender:genderRef.current.value,
+      employedDate:empDateRef.current.value,
+
+
+
     }
     alert(JSON.stringify(data))
     apiInstance
-      .post('user', data)
+      .post('user', data,{headers:{
+        "Content-Type":"multipart/form-data",
+      }})
       .then(
-
-        () => {
-          alert('success')
+function(){
+             Swal.fire({
+      icon: 'success',
+      title: 'Login Successful',
+      text: 'Welcome back!',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#3085d6',
+    });
    
-          navigate('/emp')
         })
       .catch(
-        () => { alert('error') }
+        (error) => { alert(error) }
       )
 
   };
   return (
-    <form className="w-full flex flex-col gap-4" onSubmit={create}>
+    <div className="w-full flex flex-col gap-4">
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <Input
           type="text"
           label="Name"
           placeholder="Name"
-          ref={emailRef}
+          ref={nameRef}
           variant={variant}
           labelPlacement="outside"
         />
         <Input
           type="number"
           label="Phone No"
-          ref={passRef}
+          ref={phoneRef}
           placeholder="Phone Number"
           variant={variant}
           labelPlacement="outside"
@@ -62,6 +116,7 @@ export default function EmployeeInput() {
         <Input
           type="date"
           label="Age/DOB"
+          ref={DOBRef}
           placeholder="you@example.com"
           labelPlacement="outside"
           variant={variant}
@@ -83,16 +138,18 @@ export default function EmployeeInput() {
           label="Passport No"
           placeholder="Passport Number.."
           labelPlacement="outside"
+          ref={passportRef}
           variant={variant}
         />
         <div className="block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
           <label className="text-sm font-semibold">Gender</label>
           <select
+          ref={genderRef}
             id="countries"
             className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-xl m-0 px-0 py-2 focus:ring-gray-500 focus:border-gray-500 block w-full p-3 dark:bg-default-100 dark:border-gray-600 dark:placeholder-gray-100 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
             <option hidden>Choose Gender</option>
-            <option value="US">Male</option>
-            <option value="CA">Female</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
 
           </select>
         </div>
@@ -102,6 +159,7 @@ export default function EmployeeInput() {
           isRequired
           type="email"
           variant={variant}
+          ref={emailRef}
           label="Personal Email"
           placeholder=" "
           labelPlacement="outside"
@@ -109,6 +167,7 @@ export default function EmployeeInput() {
         <Input
           type="text"
           label="Password"
+          ref={passRef}
           variant={variant}
           placeholder="Password.."
           labelPlacement="outside"
@@ -122,6 +181,7 @@ export default function EmployeeInput() {
             label="Address"
             placeholder="Address.."
             labelPlacement="outside"
+            ref={addressRef}
             variant={variant}
           />
         </div>
@@ -130,6 +190,7 @@ export default function EmployeeInput() {
             type="file"
             label="CV"
             variant={variant}
+            onChange={handlefile}
             placeholder=" "
             labelPlacement="outside"
           />
@@ -138,8 +199,9 @@ export default function EmployeeInput() {
       </div>
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <Input
-          type="email"
+          type="text"
           variant={variant}
+          ref={EuBackRef}
           label="Education Background"
           placeholder=" "
           labelPlacement="outside"
@@ -148,6 +210,7 @@ export default function EmployeeInput() {
           type="file"
           label="Education Certificate"
           variant={variant}
+          ref={EuCerRef}
           placeholder=" "
           labelPlacement="outside"
         />
@@ -155,6 +218,7 @@ export default function EmployeeInput() {
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <Input
           type="date"
+          ref={firstInRef}
           label="First Interview Date"
           placeholder="you@example.com"
           labelPlacement="outside"
@@ -163,6 +227,7 @@ export default function EmployeeInput() {
         <Input
           type="text"
           label="First Interview Result"
+          ref={firstResRef}
           placeholder="..."
           labelPlacement="outside"
           variant={variant}
@@ -174,6 +239,7 @@ export default function EmployeeInput() {
           <Input
             type="date"
             label="Second Interview Date"
+            ref={secInRef}
             placeholder=" "
             labelPlacement="outside"
             variant={variant}
@@ -182,6 +248,7 @@ export default function EmployeeInput() {
         <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
           <Input
             type="text"
+            ref={secResRef}
             label="Second Interview Result"
             variant={variant}
             placeholder="..."
@@ -223,12 +290,14 @@ export default function EmployeeInput() {
           variant={variant}
           label="Father Name"
           placeholder=" "
+          ref={fatherRef}
           labelPlacement="outside"
         />
         <Input
           type="date"
           label="Employed Date"
           placeholder=" "
+          ref={empDateRef}
           labelPlacement="outside"
           variant={variant}
         />
@@ -239,6 +308,7 @@ export default function EmployeeInput() {
           type="tel"
           variant={variant}
           label="Emergecy Contact"
+          ref={ECRef}
           placeholder=" "
           labelPlacement="outside"
         />
@@ -257,6 +327,7 @@ export default function EmployeeInput() {
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <Input
           type="text"
+          ref={workExpRef}
           label="Work Experience"
           placeholder=" "
           labelPlacement="outside"
@@ -274,7 +345,7 @@ export default function EmployeeInput() {
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <div className="block w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
           <label className="text-sm font-semibold">Leave Entitled</label>
-          <RadioGroup isRequired orientation="horizontal">
+          <RadioGroup orientation="horizontal">
             <Radio value="ca">Casul</Radio>
             <Radio value="me">Medical</Radio>
             <Radio value="va">Vacation</Radio>
@@ -283,7 +354,7 @@ export default function EmployeeInput() {
         </div>
         <div className="block w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
           <label className="text-sm font-semibold">Meal Allowance</label>
-          <RadioGroup isRequired orientation="horizontal">
+          <RadioGroup orientation="horizontal">
             <Radio value="buenos-aires">Yes</Radio>
             <Radio value="sydney">No</Radio>
           </RadioGroup>
@@ -292,14 +363,14 @@ export default function EmployeeInput() {
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <div className="block w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
           <label className="text-sm font-semibold">Married</label>
-          <RadioGroup isRequired orientation="horizontal">
+          <RadioGroup orientation="horizontal">
             <Radio value="buenos-aires">Yes</Radio>
             <Radio value="sydney">No</Radio>
           </RadioGroup>
         </div>
         <div className="block w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
           <label className="text-sm font-semibold">Travel Allowance</label>
-          <RadioGroup isRequired orientation="horizontal">
+          <RadioGroup orientation="horizontal">
             <Radio value="buenos-aires">Yes</Radio>
             <Radio value="sydney">No</Radio>
           </RadioGroup>
@@ -308,7 +379,7 @@ export default function EmployeeInput() {
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
         <div className="block w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
           <label className="text-sm font-semibold">Yearly Bonus</label>
-          <RadioGroup isRequired orientation="horizontal">
+          <RadioGroup orientation="horizontal">
             <Radio value="1">Yes</Radio>
             <Radio value="2">No</Radio>
 
@@ -316,7 +387,7 @@ export default function EmployeeInput() {
         </div>
         <div className="block w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
           <label className="text-sm font-semibold">Incentive</label>
-          <RadioGroup isRequired orientation="horizontal">
+          <RadioGroup orientation="horizontal">
             <Radio value="1">Yes</Radio>
             <Radio value="2">No</Radio>
 
@@ -326,6 +397,7 @@ export default function EmployeeInput() {
       <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
     <Input
           type="file"
+          ref={recLetterRef}
           label="Recommendation Letter"
           placeholder=" "
           labelPlacement="outside"
@@ -345,10 +417,10 @@ export default function EmployeeInput() {
       <div className="flex justify-center w-full flex-wrap md:flex-nowrap mb-4 md:mb-0 gap-4 mt-3">
 
         <Button size='sm' color='primary' variant='shadow'
-          className='rounded-xl px-4 py-0 text-left' type='submit'>Register</Button>
+          className='rounded-xl px-4 py-0 text-left' onClick={create}>Register</Button>
         <Button size='sm' color='primary' variant='shadow'
           className='rounded-xl px-4 py-0 text-left'>Cancel</Button>
       </div>
-    </form>
+    </div>
   );
 }
--- src/pages/Employee/employeeList.jsx
+++ src/pages/Employee/employeeList.jsx
@@ -1,7 +1,13 @@
 import Sidebar from "../../components/Sidebar";
-import { Card, CardHeader, CardBody, CardFooter, Divider } from "@nextui-org/react";
-import Table from '../../components/Table/table'
-import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+} from "@nextui-org/react";
+import Table from "../../components/Table/table";
+import { Link } from "react-router-dom";
 
 import { useEffect } from "react";
 import apiInstance from "../../util/api";
@@ -16,43 +22,47 @@ export default function Employee() {
     getEmployeeLists();
   }, []);
 
-    useEffect(() => {
-        const getEmployeeLists = async () => {
-            await apiInstance.get('users').then(response => { console.log(response.data.data, 'here') })
-        }
-        getEmployeeLists()
-    }, [])
+  useEffect(() => {
+    const getEmployeeLists = async () => {
+      await apiInstance.get("users").then((response) => {
+        console.log(response.data.data, "here");
+      });
+    };
+    getEmployeeLists();
+  }, []);
 
-    return (
-        <div className='flex'>
-            <div className="sidebar"><Sidebar /></div>
-            <div className="py-3 flex-grow">
-                <div className="py-1">
-                    <Card className="rounded-sm shadow-md min-h-[890px]">
-                        <CardHeader className="justify-between">
-                            <div></div>
-                            <div className='font-semibold'>Employee List</div>
-                            <Link to='/emp-add' className="bg-blue-600 px-5 py-1 rounded-md hover:bg-blue-400 hover:shadow-xl text-white">
-                                <span className="text-sm">Add</span>
-                            </Link>
-
-
-                        </CardHeader>
-                        <Divider></Divider>
-                        <CardBody className="">
-                            <Table />
-                        </CardBody>
-                        <Divider></Divider>
-                        <CardFooter>
-                            Copyright © 2023-2024 <b className='text-cyan-600'>K-win Technology</b> .All rights reserved.
-                        </CardFooter>
-                    </Card>
-                </div>
-            </div>
-
-
+  return (
+    <>
+      <div className="flex">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="py-3 flex-grow">
+          <div className="py-1">
+            <Card className="rounded-sm shadow-md min-h-[890px]">
+              <CardHeader className="justify-between">
+                <div></div>
+                <div className="font-semibold">Employee List</div>
+                <Link
+                  to="/emp-add"
+                  className="bg-blue-600 px-5 py-1 rounded-md hover:bg-blue-400 hover:shadow-xl text-white">
+                  <span className="text-sm">Add</span>
+                </Link>
+              </CardHeader>
+              <Divider></Divider>
+              <CardBody className="">
+                <Table />
+              </CardBody>
+              <Divider></Divider>
+              <CardFooter>
+                Copyright © 2023-2024{" "}
+                <b className="text-cyan-600">K-win Technology</b> .All rights
+                reserved.
+              </CardFooter>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
