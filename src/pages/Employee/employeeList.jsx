05e745bf1d8efeedc104a4c53bc2f1cb0ea61fc5
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2023-08-22 17:48:45 +0630

    fix

--- src/pages/Employee/employeeList.jsx
+++ src/pages/Employee/employeeList.jsx
@@ -1,61 +1,68 @@
-import Sidebar from "../../components/Sidebar";
+import Sidebar from '../../components/Sidebar'
 import {
   Card,
   CardHeader,
   CardBody,
   CardFooter,
   Divider,
-} from "@nextui-org/react";
-import Table from "../../components/Employee/employeeList";
-import { Link } from "react-router-dom";
+  Button
+} from '@nextui-org/react'
+import { BsPlusSquareDotted } from 'react-icons/bs'
 
-import { useEffect } from "react";
-import apiInstance from "../../util/api";
+import Table from '../../components/Employee/employeeList'
+import { Link } from 'react-router-dom'
 
-export default function Employee() {
+import { useEffect } from 'react'
+import apiInstance from '../../util/api'
+
+export default function Employee () {
   useEffect(() => {
     const getEmployeeLists = async () => {
-      await apiInstance.get("users").then((response) => {
-        console.log(response.data.data, "here");
-      });
-    };
-    getEmployeeLists();
-  }, []);
+      await apiInstance.get('users').then(response => {
+        console.log(response.data.data, 'here')
+      })
+    }
+    getEmployeeLists()
+  }, [])
 
   useEffect(() => {
     const getEmployeeLists = async () => {
-      await apiInstance.get("users").then((response) => {
-        console.log(response.data.data, "here");
-      });
-    };
-    getEmployeeLists();
-  }, []);
+      await apiInstance.get('users').then(response => {
+        console.log(response.data.data, 'here')
+      })
+    }
+    getEmployeeLists()
+  }, [])
 
   return (
     <>
-      <div className="flex">
-        <div className="sidebar">
+      <div className='flex'>
+        <div className='sidebar'>
           <Sidebar />
         </div>
-        <div className=" flex-grow">
-          <div className="">
-            <Card className="rounded-sm shadow-md py-3 min-h-[890px]">
-              <CardHeader className="justify-between">
+        <div className=' flex-grow'>
+          <div className=''>
+            <Card className='rounded-sm shadow-md py-3 min-h-[890px]'>
+              <CardHeader className='justify-between'>
                 <div></div>
-                <div className="font-semibold">Employee List</div>
-                <Link
-                  to="/emp-add"
-                  className="bg-blue-600 px-5 py-1 rounded-md hover:bg-blue-400 hover:shadow-xl text-white ">
-                  <span className="text-sm ">Add</span>
+                <div className='font-semibold'>Employee List</div>
+                <Link to='/emp-add'>
+                  <Button
+                    endContent={<BsPlusSquareDotted />}
+                    color='primary'
+                    className='text-sm '
+                  >
+                    Add
+                  </Button>
                 </Link>
               </CardHeader>
-              <CardBody className="">
+              <CardBody className=''>
                 <Table />
               </CardBody>
               <Divider></Divider>
               <CardFooter>
-                Copyright © 2023-2024{" "}
-                <b className="text-cyan-600">K-win Technology</b> .All rights
+                Copyright © 2023-2024{' '}
+                <b className='text-cyan-600'>K-win Technology</b> .All rights
                 reserved.
               </CardFooter>
             </Card>
@@ -63,5 +70,5 @@ export default function Employee() {
         </div>
       </div>
     </>
-  );
+  )
 }
