0bfd677093a3be2a8c68f5128a75d39a7f6ac74e
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2023-08-22 14:59:57 +0630

    fix

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/login';
-import Nav from './components/Navbar/index';
-import Dashboard from './pages/Dashboard/index.jsx';
-import Employee from './pages/Employee/employeeList';
-import EmployeeAdd from './pages/Employee/employeeAdd';
-import EmployeeUpdate from './pages/Employee/employeeDetail';
-import Position from './pages/Position/Position.jsx';
-import AuthContainer from './util/AuthContainer.jsx';
-import PositionRegsiter from './pages/Position/Add';
-import Department from './pages/Department/Department';
-import DepartmentRegister from './pages/Department/DepartmentRegister';
-import DepartmentChart from './pages/Department/DepartmentChart';
-import PositionUpdate from './pages/Position/PositionUpdate';
-import DepartmentUpdate from './pages/Department/DepartmentUpdate';
-import Attendance from './pages/Attendance/Attendance';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/login'
+import Nav from './components/Navbar/index'
+import Dashboard from './pages/Dashboard/index.jsx'
+import Employee from './pages/Employee/employeeList'
+import EmployeeAdd from './pages/Employee/employeeAdd'
+import EmployeeUpdate from './pages/Employee/employeeDetail'
+import Position from './pages/Position/Position.jsx'
+import AuthContainer from './util/AuthContainer.jsx'
+import PositionRegsiter from './pages/Position/Add'
+import Department from './pages/Department/Department'
+import DepartmentRegister from './pages/Department/DepartmentRegister'
+import DepartmentChart from './pages/Department/DepartmentChart'
+import PositionUpdate from './pages/Position/PositionUpdate'
+import DepartmentUpdate from './pages/Department/DepartmentUpdate'
+import Attendance from './pages/Attendance/Attendance'
 import AttendanceAdd from './pages/Attendance/AttendanceAdd'
+import AttendanceUpdate from './pages/Attendance/AttendanceUpdate'
 
-export default function RouteFile() {
+export default function RouteFile () {
   return (
     <>
       <BrowserRouter>
@@ -31,9 +32,18 @@ export default function RouteFile() {
 
             {/* Department */}
             <Route path='/department' element={<Department />}></Route>
-            <Route path='/department/register' element={<DepartmentRegister />}></Route>
-            <Route path='/department/chart' element={<DepartmentChart />}></Route>
-            <Route path='/department/update/:id' element={<DepartmentUpdate />}></Route>
+            <Route
+              path='/department/register'
+              element={<DepartmentRegister />}
+            ></Route>
+            <Route
+              path='/department/chart'
+              element={<DepartmentChart />}
+            ></Route>
+            <Route
+              path='/department/update/:id'
+              element={<DepartmentUpdate />}
+            ></Route>
 
             {/* Employee */}
             <Route path='/emp' element={<Employee />} />
@@ -45,15 +55,17 @@ export default function RouteFile() {
             <Route path='/position/register' element={<PositionRegsiter />} />
             <Route path='/position/update/:id' element={<PositionUpdate />} />
 
-
             {/* Attendance */}
             <Route path='/attendance' element={<Attendance />}></Route>
             <Route path='/att-add' element={<AttendanceAdd />}></Route>
+            <Route
+              path='/att-update/:id'
+              element={<AttendanceUpdate />}
+            ></Route>
           </Route>
           {/* <AuthContainer component={<Dashboard />} path='/home'></AuthContainer> */}
-
         </Routes>
-      </BrowserRouter >
+      </BrowserRouter>
     </>
   )
 }
