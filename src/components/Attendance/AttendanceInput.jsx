a938c9d925ee5f606c0eaadc9d2e8df967d03aab
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-09-14 14:50:04 +0630

    here

--- src/components/Attendance/AttendanceInput.jsx
+++ src/components/Attendance/AttendanceInput.jsx
@@ -4,12 +4,12 @@ import { useEffect, useState } from 'react'
 import Swal from 'sweetalert2'
 import { Link } from 'react-router-dom'
 
-export default function DepartmentInputForm () {
+export default function DepartmentInputForm() {
   const variant = 'faded'
   const [departmentList, setDepartmentList] = useState([])
   const [userList, setUserList] = useState([])
-  const [showAtt,setShowAtt]=useState(false)
-  const [showDis,setShowDis]=useState(false)
+  const [showAtt, setShowAtt] = useState(false)
+  const [showDis, setShowDis] = useState(false)
 
   const handleInputChange = (fieldName, value) => {
     setData(prevValues => ({
@@ -18,11 +18,11 @@ export default function DepartmentInputForm () {
     }))
   }
 
-  const handleAttDis=(e)=>{
-    if(e === 'Attend'){
+  const handleAttDis = (e) => {
+    if (e === 'Attend') {
       setShowAtt(true)
-          setShowDis(false)
-    }else{
+      setShowDis(false)
+    } else {
       setShowDis(true)
       setShowAtt(false)
     }
@@ -82,7 +82,7 @@ export default function DepartmentInputForm () {
             ))}
           </select>
         </div>
-          <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-1'>
+        <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-1'>
           <label className='text-sm font-semibold'>Date</label>
           <Input
             type='date'
@@ -90,7 +90,7 @@ export default function DepartmentInputForm () {
             onChange={e => handleInputChange('date', e.target.value)}
           />
         </div>
-      
+
       </div>
       <div className='flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-3'>
         <Input
@@ -101,7 +101,7 @@ export default function DepartmentInputForm () {
           onChange={e => handleInputChange('clockIn', e.target.value)}
           labelPlacement='outside'
         />
-      <Input
+        <Input
           type='time'
           label='ClockOut Time'
           placeholder='Time'
@@ -111,7 +111,7 @@ export default function DepartmentInputForm () {
         />
       </div>
       <div className='flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-3'>
-         <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
+        <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
           <label className='text-sm font-semibold'>Source</label>
           <select
             onChange={e => handleInputChange('source', e.target.value)}
@@ -123,7 +123,7 @@ export default function DepartmentInputForm () {
             <option value='Manual'>Manual</option>
           </select>
         </div>
-       
+
         <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
           <label className='text-sm font-semibold'>Department</label>
           <select
@@ -141,11 +141,11 @@ export default function DepartmentInputForm () {
           </select>
         </div>
       </div>
-           <div className='flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-3'>
- <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
+      <div className='flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-3'>
+        <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
           <label className='text-sm font-semibold'>Type</label>
           <select
-            onChange={e => {handleInputChange('type', e.target.value), handleAttDis(e.target.value)}}
+            onChange={e => { handleInputChange('type', e.target.value), handleAttDis(e.target.value) }}
             className='bg-gray-100 border mt-2 border-gray-300 text-gray-900 text-sm rounded-xl m-0 px-0 py-2 focus:ring-gray-500 focus:border-gray-500 block w-full p-3 dark:bg-default-100 dark:border-gray-600 dark:placeholder-gray-100 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500'
           >
             <option hidden>Choose Type</option>
@@ -157,34 +157,35 @@ export default function DepartmentInputForm () {
         <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
           {showAtt && (
             <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
-          <label className='text-sm font-semibold'>AttendType</label>
-          <select
-            onChange={e => handleInputChange('attendType', e.target.value)}
-            className='bg-gray-100 border mt-2 border-gray-300 text-gray-900 text-sm rounded-xl m-0 px-0 py-2 focus:ring-gray-500 focus:border-gray-500 block w-full p-3 dark:bg-default-100 dark:border-gray-600 dark:placeholder-gray-100 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500'
-          >
-            <option hidden>Choose Attend Type</option>
+              <label className='text-sm font-semibold'>AttendType</label>
+              <select
+                onChange={e => handleInputChange('attendType', e.target.value)}
+                className='bg-gray-100 border mt-2 border-gray-300 text-gray-900 text-sm rounded-xl m-0 px-0 py-2 focus:ring-gray-500 focus:border-gray-500 block w-full p-3 dark:bg-default-100 dark:border-gray-600 dark:placeholder-gray-100 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500'
+              >
+                <option hidden>Choose Attend Type</option>
 
-            <option value='Week Day'>Week Day</option>
-            <option value='Day Off'>Day Off</option>
-          </select>
-        </div>
+                <option value='Week Day'>Week Day</option>
+                <option value='Day Off'>Day Off</option>
+                <option value='Holiday'>Holiday</option>
+              </select>
+            </div>
           )}
 
           {showDis && (
-                <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
- <Input
-          type='text'
-          label='Dismiss Reason'
-          placeholder='Dismiss reason ...'
-          variant={variant}
-          onChange={e => handleInputChange('dismissReason', e.target.value)}
-          labelPlacement='outside'
-        />
-        </div>
+            <div className='block w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4'>
+              <Input
+                type='text'
+                label='Dismiss Reason'
+                placeholder='Dismiss reason ...'
+                variant={variant}
+                onChange={e => handleInputChange('dismissReason', e.target.value)}
+                labelPlacement='outside'
+              />
+            </div>
           )}
-           
+
         </div>
-        </div>
+      </div>
       <div className='flex justify-center gap-10 py-4'>
         <Button color='danger'>
           <Link to='/attendance'>Cancel</Link>
